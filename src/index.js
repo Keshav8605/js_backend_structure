b3c661713deebbbc92877f25809e82054b6e30d4
Keshav8605 <gargkeshav793@gmail.com>
2025-12-30 23:01:18 +0530

    kk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import CONNECT_DB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -6,6 +7,18 @@ dotenv.config({
 })
 
 
-CONNECT_DB()
+CONNECT_DB().then(
+    ()=>{
+        app.on("error",(err)=>{
+        console.log("ERROR :-",err)
+        throw error
+    })
+    app.listen(process.env.PORT || 800, ()=>{
+        console.log(`Server is running on the port :- ${process.env.PORT}`)
+    })
+    }
+).catch((err)=>{
+    console.log("DB connection failed",err);
+})
 
 console.log("App started");
